87d7173ed2999f22368ff58a0307ded5f8d25cfe
Zakki0925224 <zakki0925224@gmail.com>
2022-08-30 13:30:21 +0900

    [Add]: Added first commit

---
+++ bootloader/src/lib.rs
@@ -0,0 +1,16 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn efi_main() -> !
+{
+    loop {};
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> !
+{
+    loop {};
+}
