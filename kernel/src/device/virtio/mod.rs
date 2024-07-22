84d2698ebce030f2727628f69886388571e4b1c9
Zakki0925224 <zakki0925224@gmail.com>
2024-07-22 16:12:09 +0900

    [Todo]: Reading MMIO device register

--- kernel/src/device/virtio/mod.rs
+++ kernel/src/device/virtio/mod.rs
@@ -1,1 +1,60 @@
+use crate::addr::VirtualAddress;
+use core::mem::transmute;
+
 pub mod net;
+
+const MMIO_DEVICE_REG_MAGIC: u32 = 0x74726976;
+
+// reference: https://docs.oasis-open.org/virtio/virtio/v1.2/csd01/virtio-v1.2-csd01.html
+
+// 4.2.2 MMIO Device Register Layout
+#[derive(Debug)]
+#[repr(C)]
+struct MmioDeviceRegister {
+    /*r */ magic: u32,
+    /*r */ version: u32,
+    /*r */ device_id: u32,
+    /*r */ vendor_id: u32,
+    /*r */ device_features: u32,
+    /*w */ device_features_sel: u32,
+    /*w */ driver_features: u32,
+    /*w */ driver_features_sel: u32,
+    /*w */ queue_sel: u32,
+    /*r */ queue_num_max: u32,
+    /*w */ queue_num: u32,
+    /*rw*/ queue_ready: u32,
+    /*w */ queue_notify: u32,
+    /*r */ int_status: u32,
+    /*w */ int_ack: u32,
+    /*rw*/ status: u32,
+    /*w */ queue_desc_low: u32,
+    /*w */ queue_desc_high: u32,
+    /*w */ queue_device_low: u32,
+    /*w */ queue_device_high: u32,
+    /*w */ shm_sel: u32, // shared memory
+    /*r */ shm_len_low: u32,
+    /*r */ shm_len_high: u32,
+    /*r */ shm_base_low: u32,
+    /*r */ shm_base_high: u32,
+    /*rw*/ queue_reset: u32,
+    /*r */ conf_gen: u32,
+    // /*rw*/ config: N
+}
+
+impl MmioDeviceRegister {
+    pub fn read(base_addr: VirtualAddress) -> Self {
+        let mut data = [0; 27];
+        for (i, field) in data.iter_mut().enumerate() {
+            *field = base_addr.offset(i * 4).read_volatile();
+        }
+
+        let reg = unsafe { transmute::<[u32; 27], Self>(data) };
+        assert_eq!(reg.magic, MMIO_DEVICE_REG_MAGIC, "Invalid magic number");
+        reg
+    }
+
+    #[allow(dead_code)]
+    pub fn write(&self) {
+        unimplemented!();
+    }
+}
--- kernel/src/device/virtio/net.rs
+++ kernel/src/device/virtio/net.rs
@@ -1,6 +1,6 @@
 use crate::{
-    bus::pci::{self, vendor_id},
-    device::{DeviceDriverFunction, DeviceDriverInfo},
+    bus::pci::{self, conf_space::BaseAddress, vendor_id},
+    device::{virtio::MmioDeviceRegister, DeviceDriverFunction, DeviceDriverInfo},
     error::{Error, Result},
     println,
     util::mutex::Mutex,
@@ -47,41 +47,56 @@ impl DeviceDriverFunction for VirtioNetDriver {
 
         let (bus, device, func) = self.pci_device_bdf.unwrap();
         pci::configure_device(bus, device, func, |d| {
-            let mut c_common = None;
-            let mut c_notify = None;
-            let mut c_isr = None;
-            let mut c_device = None;
-            let mut c_pci = None;
-            let mut c_memory = None;
-            let mut c_vendor = None;
+            let conf_space = d.read_conf_space_non_bridge_field()?;
+            let bars = conf_space.get_bars()?;
+            let (_, mmio_bar) = bars
+                .get(1)
+                .ok_or(Error::Failed("Failed to read MMIO base address register"))?;
+            let mmio_addr = match mmio_bar {
+                //BaseAddress::MmioAddressSpace(addr) => (*addr as u64).into(),
+                BaseAddress::MemoryAddress32BitSpace(phy_addr, _) => phy_addr.get_virt_addr(),
+                _ => return Err(Error::Failed("Invalid base address register")),
+            }?;
 
-            let caps_list = d.read_msi_caps_list();
-            for c in caps_list {
-                let ty = (c.msg_ctrl.raw() >> 8) as u8;
+            // TODO: magic number is 0
+            let dev_reg = MmioDeviceRegister::read(mmio_addr);
+            println!("{:?}", dev_reg);
 
-                if c.cap_id != 0x09 {
-                    continue;
-                }
+            // let mut c_common = None;
+            // let mut c_notify = None;
+            // let mut c_isr = None;
+            // let mut c_device = None;
+            // let mut c_pci = None;
+            // let mut c_memory = None;
+            // let mut c_vendor = None;
 
-                match ty {
-                    0x01 => c_common = Some(c),
-                    0x02 => c_notify = Some(c),
-                    0x03 => c_isr = Some(c),
-                    0x04 => c_device = Some(c),
-                    0x05 => c_pci = Some(c),
-                    0x08 => c_memory = Some(c),
-                    0x09 => c_vendor = Some(c),
-                    _ => (),
-                }
-            }
+            // let caps_list = d.read_msi_caps_list();
+            // for c in caps_list {
+            //     let ty = (c.msg_ctrl.raw() >> 8) as u8;
 
-            println!("common: {:?}", c_common);
-            println!("notify: {:?}", c_notify);
-            println!("isr: {:?}", c_isr);
-            println!("device: {:?}", c_device);
-            println!("pci: {:?}", c_pci);
-            println!("memory: {:?}", c_memory);
-            println!("vendor: {:?}", c_vendor);
+            //     if c.cap_id != 0x09 {
+            //         continue;
+            //     }
+
+            //     match ty {
+            //         0x01 => c_common = Some(c),
+            //         0x02 => c_notify = Some(c),
+            //         0x03 => c_isr = Some(c),
+            //         0x04 => c_device = Some(c),
+            //         0x05 => c_pci = Some(c),
+            //         0x08 => c_memory = Some(c),
+            //         0x09 => c_vendor = Some(c),
+            //         _ => (),
+            //     }
+            // }
+
+            // println!("common: {:?}", c_common);
+            // println!("notify: {:?}", c_notify);
+            // println!("isr: {:?}", c_isr);
+            // println!("device: {:?}", c_device);
+            // println!("pci: {:?}", c_pci);
+            // println!("memory: {:?}", c_memory);
+            // println!("vendor: {:?}", c_vendor);
 
             Ok(())
         })?;
