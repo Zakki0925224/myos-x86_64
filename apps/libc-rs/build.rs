4c78ae89ffb3cca52f62c0edbe28794175e42714
Zakki0925224 <zakki0925224@gmail.com>
2024-11-30 23:49:20 +0900

    [Fix]: Fixed `build.rs` for libc-rs

--- apps/libc-rs/build.rs
+++ apps/libc-rs/build.rs
@@ -1,23 +1,43 @@
+use std::{fs, path::PathBuf};
+
+fn find_headers_recursively(dir: PathBuf) -> Vec<PathBuf> {
+    let mut headers = Vec::new();
+
+    for entry in fs::read_dir(&dir).expect("Failed to read directory") {
+        let entry = entry.expect("Failed to read directory entry");
+        let path = entry.path();
+
+        if path.is_dir() {
+            headers.extend(find_headers_recursively(path));
+        } else if let Some(ext) = path.extension() {
+            if ext == "h" {
+                headers.push(path);
+            }
+        }
+    }
+
+    headers
+}
+
 fn main() {
     println!("cargo::rustc-link-search=../libc");
     println!("cargo::rustc-link-lib=c");
 
-    let bindings = bindgen::Builder::default()
-        .header("../libc/sys/stat.h")
-        .header("../libc/ctype.h")
-        .header("../libc/stat.h")
-        .header("../libc/stdio.h")
-        .header("../libc/stdlib.h")
-        .header("../libc/string.h")
-        .header("../libc/syscalls.h")
-        .header("../libc/utsname.h")
-        .header("../libc/window.h")
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let out_path = std::path::PathBuf::from(out_dir);
+
+    let libc_path = PathBuf::from("../libc");
+    let headers = find_headers_recursively(libc_path);
+    let mut builder = bindgen::Builder::default();
+
+    for header in headers {
+        builder = builder.header(header.to_str().unwrap());
+    }
+
+    let bindings = builder
         .use_core()
         .generate()
         .expect("Failed to generate bindings");
-
-    let out_dir = std::env::var("OUT_DIR").unwrap();
-    let out_path = std::path::PathBuf::from(out_dir);
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Fialed to wrote bindings");
